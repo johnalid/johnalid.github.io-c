b426cfcdb1ba6950384eecc2ce3538c4b78f5e15
johnalid <johnalidy05@gmail.com>
2021-05-16 15:42:02 -0600

    remade

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+const tog =document.querySelector(".tog");
+const hd =document.querySelector(".hide");
 
 
 
+tog.addEventListener("click", ()=>{
+    if(hd.classList.contains("active")){
+        hd.classList.remove("active")
+    }else{
+        hd.classList.add("active")
+    }
+});
+
+window.addEventListener("scroll", ()=>{
+const showed = document.querySelector(".top");
+showed.classList.toggle("shw", scrollY >= 40);
+
+}) 
+
+
+// for hide1
 
 
